06233ace02afdde52512cfa51de5bdaf031b70d2
yvnlarrye <eugenymc07@gmail.com>
2023-12-19 01:29:16 +0300

    removed unused methods from user model

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -18,16 +18,8 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
